4fab779b71453d540131207d39e3da23a5681a3c
Jayakumar <lowpowdeveloper@gmail.com>
2026-01-28 12:57:42 +0530

    Update card titles and URLs for improved clarity and functionality

--- urls.js
+++ urls.js
@@ -5,6 +5,7 @@ window.URLS = {
     "TNRD": "https://tnrd.tn.gov.in/project/home.php",
     "Karuvoolam": "https://www.karuvoolam.tn.gov.in/web/tnta/",
     "WhatsApp": "https://web.whatsapp.com/",
+    "IPMS": "https://ipms.tnrd.tn.gov.in/login.php",
 };
 
 // keep for future extension
