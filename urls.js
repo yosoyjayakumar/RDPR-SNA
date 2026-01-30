782c128c54775347e26c14b3274ac97c35fa0b67
Jayakumar <lowpowdeveloper@gmail.com>
2026-01-30 18:36:45 +0530

    Update card data URLs and titles for A2 sections to enhance navigation and functionality

--- urls.js
+++ urls.js
@@ -6,6 +6,15 @@ window.URLS = {
     "Karuvoolam": "https://www.karuvoolam.tn.gov.in/web/tnta/",
     "WhatsApp": "https://web.whatsapp.com/",
     "IPMS": "https://ipms.tnrd.tn.gov.in/login.php",
+
+    //A2
+    "SIDS, RBIOCGS & CFSIDS": "https://tnrdprsis.iob",
+    "MLACDS": "https://tnrdmlacds.iob.bank.in/login",
+    "NNT": "https://tnrdnnt.iob.bank.in/login",
+    "MPLADS": "https://mplads.sbi/digigov/Login.zul",
+    "MI Tank": "https://tnagamt.indianbank.bank.in/select-scheme",
+    "NABARD": "https://drdasna.rbl.bank.in/login",
+
 };
 
 // keep for future extension
