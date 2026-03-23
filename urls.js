6441a852911b73d22ea0dfcdb69bb5d7d80f463c
Jayakumar <lowpowdeveloper@gmail.com>
2026-03-23 11:43:33 +0530

    Refactor code structure for improved readability and maintainability

--- urls.js
+++ urls.js
@@ -5,14 +5,17 @@ window.URLS = {
     "TNRD": "https://tnrd.tn.gov.in/project/home.php",
     "Karuvoolam": "https://www.karuvoolam.tn.gov.in/web/tnta/",
     "WhatsApp": "https://web.whatsapp.com/",
-    "IPMS": "https://ipms.tnrd.tn.gov.in/login.php",
     "IlovePDF": "https://www.ilovepdf.com/",
     "PDF.io": "https://pdf.io/",
+    "11zon": "https://www.11zon.com/",
 
     //Scheme Side
     "AGAMT": "https://tnagamt.indianbank.bank.in/select-scheme",
     "VPTAX": "https://vptax.tnrd.tn.gov.in/",
     "TNPASS": "https://tndrdpri.indianbankuat.bank.in/login",
+
+    //A1 Section
+    "E2E": "https://ipms.tnrd.tn.gov.in/login.php",
 
     //A2
     "SIDS, RBIOCGS & CFSIDS": "https://tnrdprsis.iob.bank.in/login",
@@ -22,14 +25,20 @@ window.URLS = {
     "MI Tank": "https://tnagamt.indianbank.bank.in/select-scheme",
     "NABARD": "https://drdasna.rbl.bank.in/login",
 
+    //A3
+    "15thfc": "https://egramswaraj.gov.in/",
+
     //A4
     "GST": "https://services.gst.gov.in/services/login",
     "IT": "https://www.incometax.gov.in/iec/foportal/",
-    "Cams": "https://cams.tn.gov.in/",
-    
+    "CAMS": "https://cams.tn.gov.in/",
 
     //A5
     "Zoho": "https://www.zoho.com/desk/login.html",
     "ooratchimani": "https://ooratchimani.org/#/login-link",
 
+    //Random
+    "Speed Test": "https://fast.com/#",
+    "Number2Words": "https://num2word.com/",
+    "unicode2suntommy": "https://tamilfontconverter.com/unicode-to-suntommy/"
 };
